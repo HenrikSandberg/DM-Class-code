7011b85a483271fc91eb2784988e7cfbce2d8178
Henrik sandberg <henrik.a.sandberg@icloud.com>
2019-10-08 15:06:23 +0200

    Some more assignemts

--- forelesning1/script.js
+++ forelesning1/script.js
@@ -10,6 +10,7 @@ navn = "Meea";
 
 // Number
 // Alle heltall og desimaltall
+// Int, short, long og dobble og float
 var tall = 123;
 
 // + - * / %
@@ -27,6 +28,9 @@ console.log(navn + " og liker tallet " + tall);
 var erStudent = false;
 var erProfessor = false;
 
+// Ukjent og udefinert
+var ukjent = undefined;
+var absoluttNull = null;
 
 // IF ELSE
 if (erStudent) {
@@ -36,6 +40,56 @@ if (erStudent) {
 } else {
     console.log("Du er ikke ansatt eller student. Hvem er du!");
 }
+
+
+// Boolske Opperasjoner
+// < minder enn
+// > større enn
+// <= mindre eller lik
+// >= større eller lik
+// !== ikke lik
+// === lik 
+
+var alder;
+var aldersgrense = 20;
+var nyttNavn = null;
+
+
+alder = prompt("Tast inn din alder");
+
+if (parseInt(alder) >= aldersgrense) {
+    alert("Velkommen!");
+} else {
+    alert("Du er ikke gammel nok desverre");
+}
+
+// LØKKER
+// While
+var teller = 0;
+
+while (teller < 10) {
+    console.log(teller);
+    teller = teller + 1;
+}
+
+console.log("Etter løkka er kjørt så er teller = " + teller);
+
+while (nyttNavn === null) {
+    nyttNavn = prompt("Vendligst oppgi navnet ditt");
+
+    if (nyttNavn !== null) {
+        alert("Velkommen " + nyttNavn);
+    }   
+}
+
+// FOR
+// teller, et boolsk utrykk og en oppdatering av telleren
+for (var nyTeller = 0; nyTeller < 10; nyTeller = nyTeller + 1) {
+    console.log(nyTeller);
+}
+for (var i = 0; i < 10; i++){
+    
+} 
 
 
 
--- oppgaver1/assignment.js
+++ oppgaver1/assignment.js
@@ -37,8 +37,58 @@ var y = 8;
 //
 
 
+//6.
+/* Skriv med tekst hva som vil komme ut av denne løkka. Ta løkka inn i consol i chrome og se om det som kom ut var det samme som du forventet
+*/
+var num = 1;
 
+while(num <= 10) {
+  console.log(num);
+  num += 2;
+}
 
+//SVAR:
+/*
+
+*/
+
+//7.
+/* Skriv med tekst hva som vil komme ut av denne løkka. Ta løkka inn i consol i chrome og se om det som kom ut var det samme som du forventet
+*/
+var num = 1
+
+while(num <= 20) {
+ if(num % 4 === 0){
+   console.log(num);
+ }
+ num++;
+}
+
+//8.
+/* Skriv med tekst hva som vil komme ut av denne løkka. Ta løkka inn i consol i chrome og se om det som kom ut var det samme som du forventet
+*/
+var num = 100;
+
+while(num < 150) {
+  console.log(num + 1);
+  num--;
+}
+
+//9.
+/* Skriv med tekst hva som vil komme ut av denne løkka. Ta løkka inn i consol i chrome og se om det som kom ut var det samme som du forventet
+*/
+for(var i = 0; i < 16; i+=8){
+  console.log(i);
+}
+
+//10.
+/* Skriv med tekst hva som vil komme ut av denne løkka. Ta løkka inn i consol i chrome og se om det som kom ut var det samme som du forventet
+*/
+var str = "ahceclwlxo";
+
+for(var i = 1; i < str.length; i+=2){
+  console.log(str[i]);
+}
 
 
 
