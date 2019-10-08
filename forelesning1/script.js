6eb9da01a843604edae2670f209b5f3f388205c6
Henrik sandberg <henrik.a.sandberg@icloud.com>
2019-10-08 14:13:22 +0200

    first lecture

---
+++ forelesning1/script.js
@@ -0,0 +1,61 @@
+
+// Dette er en kommentar og vil ikke bli kjørt som kode. 
+// De er ment å vise til dere tekst 
+
+// Text eller string
+// Alt tekst innhold. For å deklarere at noe er tekst bruker vi ''  
+// eller ""
+var navn = "Henrik";
+navn = "Meea";
+
+// Number
+// Alle heltall og desimaltall
+var tall = 123;
+
+// + - * / %
+tall = 1;
+tall = tall + tall;
+tall * 1000;
+
+console.log("Hei på deg!");
+console.log(tall);
+console.log(navn);
+console.log(navn + " og liker tallet " + tall);
+
+// Boolean - Bools uttrykk
+// Noe er sant eller usant - 1 eller 0
+var erStudent = false;
+var erProfessor = false;
+
+
+// IF ELSE
+if (erStudent) {
+    console.log("Hei kjære student");
+} else if (erProfessor) {
+    console.log("God dag professor");
+} else {
+    console.log("Du er ikke ansatt eller student. Hvem er du!");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ oppgaver1/assignment.js
@@ -0,0 +1,49 @@
+// Prøv å gjett hva disse blir med å skrive det i en kommentar under
+// Prøv å legg dem inn i consolen i Chrome og se om du hadde rett
+
+//1.
+100 % 3
+
+//SVAR:
+//
+
+//2.
+("blah" + "blah")[6]
+
+//SVAR:
+//
+
+//3.
+"hello".length % "hi\\".length
+
+//SVAR:
+//
+
+//4.
+var x = 10;
+var y = "a"
+
+y === "b" || x >= 10
+//SVAR:
+//
+
+
+//5. 
+var x = 3;
+var y = 8;
+
+!(x == "3" || x === y) && !(y != 8 && x <= y)
+//SVAR:
+//
+
+
+
+
+
+
+
+
+
+
+
+
